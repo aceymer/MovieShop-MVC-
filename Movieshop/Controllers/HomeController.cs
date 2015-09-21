521cdbaa3724a3aea63c00ad3a61425e0917cfd6
janx360g <janx360g@easv365.dk>
2015-09-21 10:44:25 +0200

    HomeController Test

--- Movieshop/Controllers/HomeController.cs
+++ Movieshop/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Movieshop.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Hello Lars.";
 
             return View();
         }
