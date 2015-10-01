a42963b5d6e046cac6fc89e289e6af717a9917bb
aceymer <ljb@easv.dk>
2015-10-01 13:13:51 +0200

    Hibbu bubbi

--- Movieshop/Global.asax.cs
+++ Movieshop/Global.asax.cs
@@ -10,7 +10,8 @@ namespace Movieshop
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-        protected void Application_Start()
+ //Bingo bango banan
+       protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
